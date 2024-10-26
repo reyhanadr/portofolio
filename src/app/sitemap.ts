f3e89ae03afffba9cb90ae5be3bd5225d4815319
reyhanadr <reyhan.a.deris@gmail.com>
2024-10-26 20:45:56 +0700

    fix sitemap v2

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,27 +5,28 @@ import { routing } from '@/i18n/routing'
 export default async function sitemap() {
 
     const locales = routing.locales;
+    const fullBaseURL = `https://${baseURL}`; // Tambahkan "https://" ke baseURL
 
     let blogs = locales.flatMap((locale) => 
         getPosts(['src', 'app', '[locale]', 'blog', 'posts', locale]).map((post) => ({
-            url: `${baseURL}/${locale}/blog/${post.slug}`,
+            url: `${fullBaseURL}/${locale}/blog/${post.slug}`,
             lastModified: post.metadata.publishedAt,
         }))
     );
 
     let works = locales.flatMap((locale) => 
         getPosts(['src', 'app', '[locale]', 'work', 'projects', locale]).map((post) => ({
-            url: `${baseURL}/${locale}/work/${post.slug}`,
+            url: `${fullBaseURL}/${locale}/work/${post.slug}`,
             lastModified: post.metadata.publishedAt,
         }))
     );
 
-    let routes = locales.flatMap((locale)=> 
+    let routes = locales.flatMap((locale) => 
         ['', '/blog', '/work'].map((route) => ({
-            url: `${baseURL}/${locale}${route}`,
+            url: `${fullBaseURL}/${locale}${route}`,
             lastModified: new Date().toISOString().split('T')[0],
         }))
     );
 
-    return [...routes, ...blogs, ...works]
+    return [...routes, ...blogs, ...works];
 }
